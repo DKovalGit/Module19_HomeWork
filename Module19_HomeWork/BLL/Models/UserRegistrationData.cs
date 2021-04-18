f01170d0c383d71739210a766e9bff9b5fa76c44
DKovalGit <dikoval@yandex.ru>
2021-04-19 02:36:43 +0300

    Module19_HomeWork_v1

---
+++ Module19_HomeWork/BLL/Models/UserRegistrationData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.BLL.Models
+{
+    // здесь будут находится все свойства для регистрации пользователя
+    public class UserRegistrationData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
