b158d32e9992300bdfc2d4ef367b6a7fd753ea14
DKovalGit <dikoval@yandex.ru>
2021-04-21 02:17:25 +0300

    Module19_HomeWork_v2

---
+++ Module19_HomeWork/BLL/Services/FriendService.cs
@@ -0,0 +1,40 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.DAL.Entities;
+using SocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace SocialNetwork.BLL.Services
+{
+    public class FriendService
+    {
+
+        IFriendRepository friendRepository;
+        IUserRepository userRepository;
+
+        public FriendService()
+        {
+            userRepository = new UserRepository();
+            friendRepository = new FriendRepository();
+        }
+
+        public int FindByEmail(string friendEmail)
+        {
+            var findUserEntity = userRepository.FindByEmail(friendEmail);
+
+            if (findUserEntity is null) throw new UserNotFoundException();
+
+            return findUserEntity.id;
+        }
+        public void Create(string friendEmail, User user)
+        {
+            var user_id = FindByEmail(friendEmail);
+            if (user_id == user.Id) throw new WrongFriendException();
+            
+        }
+
+    }
+}
---
+++ Module19_HomeWork/PLL/Views/FriendCreateView.cs
@@ -0,0 +1,47 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SocialNetwork.PLL.Views
+{
+    public class FriendCreateView
+    {
+        
+        FriendService friendService;
+        public FriendCreateView(FriendService friendService)
+        {        
+            this.friendService = friendService;
+        }
+        public void Show(User user)
+        {
+            var friend = new Friend();
+            
+            Console.Write("Для добавления друга введите его E-Mail:");
+            friend.Email = Console.ReadLine();
+
+            try
+            {
+                friendService.Create(friend.Email, user);
+            }
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователя с таким E-Mail не существует.");
+            }
+            catch (WrongFriendException)
+            {
+                AlertMessage.Show("Нельзя добавить в друзья самого себя.");
+            }
+            catch (Exception)
+            {
+                AlertMessage.Show("Произошла ошибка при добавлении друга.");
+            }
+
+        }
+
+    }
+}
